10be385058318247b0ccc3f6b6d4ef6a4a47e072
hung222 <90371118+hung222@users.noreply.github.com>
2022-06-28 12:59:40 +0700

    Update index.js

--- kimik/index.js
+++ kimik/index.js
@@ -1,50 +1,11 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
+var express = require("express");
 var app = express();
+app.set("view engine", "pug"0);
+app.set("views", "./views");
+app.listen(process.env.PORT || 3000);
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-app.listen(process.env.PORT || 3000)
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.get("/", function(req, res){
+  res.render("trangchu.html");
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-app.get('/', (req, res) => {
-  res.render('index.pug')
-});
-app.get('/', (req, res) => {
-  res.render('index', {
-      title: 'Homepage'
-});});
-app.get('/', (req, res) => {
-  res.render("tc.html") 
-});});
-module.exports = app;
+        
